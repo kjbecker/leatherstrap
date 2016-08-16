7b33209dd3e56cc85abedd967d18edf46327285b
Kyle James Becker <kjamesbecker@gmail.com>
2016-08-15 23:29:44 -0500

    Added class file and initial test things

---
+++ leatherstrap.class.php
@@ -0,0 +1,10 @@
+<?php
+  class Leatherstrap {
+    public $version = '0.01';
+    private $page = 'Hello my old friend';
+    
+    public function test(){
+      echo $this->page;
+    }
+  } 
+?>
